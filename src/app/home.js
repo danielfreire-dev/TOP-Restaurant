f14c7c448cf988002d30890e833a5ffca877cbf7
Daniel Freire <20461343+danielfreire-dev@users.noreply.github.com>
2024-03-15 12:59:26 +0000

    not working

--- src/app/home.js
+++ src/app/home.js
@@ -4,5 +4,5 @@ export function homePage() {
   let image = document.createElement("img");
   let divContent = document.getElementById("content");
   image.src = "/src/assets/victoria-shes-UC0HZdUitWY-unsplash.jpg";
-  //document.divContent.appendChild(image);
+  divContent.appendChild(image);
 }
